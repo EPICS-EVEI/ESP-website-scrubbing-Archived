8fb3e1b20fc76571d1314b906f87b171418035e8
raghavcast <raghavcast@gmail.com>
2022-11-17 11:00:47 -0500

    Abandoned this idea

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# ESP-website-scrubbing
+# ESP-website-scrubbing (Archived)
 Website scrubbing code for the ESP32 so that we can get data from the newly design EVEI website
+
+### This code does not do anything right now, and the idea for this has been abandoned
--- scrubber/scrubber.ino
+++ scrubber/scrubber.ino
@@ -4,6 +4,7 @@
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
+#include <WiFiManager.h>
 
 WiFiClientSecure client;
 
@@ -16,11 +17,21 @@ float coords[300];
 
 void setup() {
   Serial.begin(115200);
-  Serial.print("Connecting WiFi: ");
-  Serial.println(ssid);
+  //Serial.print("Connecting WiFi: ");
+  //Serial.println(ssid);
 
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFiManager wm;
+  bool res;
+  res = wm.autoConnect("AutoConnectAP", "password");
+
+  if(!res) {
+    Serial.println("Failed to connect");
+  }
+  else {
+    Serial.println("connectedAYYAD");
+  }
+  /*WiFi.begin(ssid, password);
   while(WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
@@ -35,6 +46,56 @@ void setup() {
   if(client.connected()) {
     client.stop();
   }
+
+  if(!client.connect(HOST_WEBSITE, 80)) {
+    Serial.println(F("Connection Failed"));
+    return;
+  }
+
+  yield();
+
+  client.print(F("GET "));
+  client.print("/");
+  client.println(F("HTTP/1.1"));
+
+  client.print(F("Host: "));
+  client.println(HOST_WEBSITE);
+  
+  client.println(F("Cache-Control: no-cache"));
+
+  if(client.println() == 0) {
+    Serial.println(F("Failed to send request"));
+    return;
+  }
+
+  char status[32] = {0};
+  client.readBytesUntil('\r', status, sizeof(status));
+  if(strcmp(status, "HTTP/1.1 200 OK") != 0) {
+    Serial.print(F("Unexpected response: "));
+    Serial.println(status);
+    return;
+  }
+
+  char endOfHeaders[] = "\r\n\r\n";
+  if(!client.find(endOfHeaders)) {
+    Serial.println(F("Invalid response"));
+    return;
+  }
+  
+  char coords[50] = {0};
+
+  char coordsString[] = "coordinates=\"";
+  if(!client.find(coordsString)) {
+    Serial.println(F("Found no coordinates"));
+    return;  
+  }
+  else {
+    client.readBytesUntil('\"', coords, sizeof(coords));
+    Serial.print(coords[0]);
+  }
+
+
+  return;*/
 }
 
 void loop() {
